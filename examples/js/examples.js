809ab46995de33906dfd29f3911feaafc6b4168a
Duc Doan <ducdhm@gmail.com>
2019-04-19 10:11:14 +0700

    Update examples

--- examples/js/examples.js
+++ examples/js/examples.js
@@ -1,18 +1,24 @@
 $(function () {
-	if ($('#msg-fa').length > 0) {
-		Msg.icon = Msg.ICONS.FONTAWESOME;
+	var example = $('example');
+	switch (example.attr('data-icon')) {
+		case 'fa':
+			Msg.icon = Msg.ICONS.FONTAWESOME;
+			break;
+			
+		case 'bs':
+			Msg.icon = Msg.ICONS.BOOTSTRAP;
+			break;
+			
+		default:
+			Msg.icon = {
+				info: 'fa fa-bath',
+				success: 'fa fa-anchor',
+				warning: 'fa fa-bell-o',
+				danger: 'fa fa-bolt'
+			};
 	}
 	
-	if ($('#msg-custom').length > 0) {
-		Msg.icon = {
-			info: 'fa fa-bath',
-			success: 'fa fa-anchor',
-			warning: 'fa fa-bell-o',
-			danger: 'fa fa-bolt'
-		};
-	}
-	
-	$('button').each(function () {
+	$('.btn').each(function () {
 		var btn = $(this);
 		var typeAlert = btn.attr('class').replace('btn btn-block btn-', '');
 		var msgStr = 'This is ' + typeAlert + ' alert content';
